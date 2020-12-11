53d99337e6201362b163d51e6bdd87f3c15d387e
japerezcarri <54781818+japerezcarri@users.noreply.github.com>
2020-12-10 21:52:04 -0500

    prueba

--- db/proyecto__db.py
+++ db/proyecto__db.py
@@ -3,3 +3,4 @@ from db.proyecto_db import  listar_proyectos
 """ antes: proyetos, usuarios """
 """ añadir usuario   al proyecto post asignar_trabajador()"""
 """ lista de proyectos  get listar_proyectos """
+""" comentario"""
