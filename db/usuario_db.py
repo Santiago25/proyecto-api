6c72bec08afe1c023c42331b48043c69b8ec43e9
mabelolaya <68984467+mabelolaya@users.noreply.github.com>
2020-12-11 19:42:13 -0500

    Modificar db usuario

--- db/usuario_db.py
+++ db/usuario_db.py
@@ -1,1 +1,37 @@
-"""modif"""
+from typing import Dict
+from pydantic import BaseModel
+
+
+class UserInDB(BaseModel): 
+
+username: str
+correo: str
+password: str
+
+database_users = Dict[str,UserInDB]
+
+database_users = {
+   "Mabel123": UserInDB(**{ username = "Mabel123",
+                            correo = "mabel.olaya.perez@gmail.com",
+                            password = "clave123"}),
+
+
+"Johana456": UserInDB(**{ username = "Johana456",
+                            correo = "johana.mintic@gmail.com",
+                            password = "clave456"}),
+
+"Santiago789": UserInDB(**{ username = "Santiago789",
+                            correo = "santiagoprogramador@gmail.com",
+                            password = "clave789"}),
+
+"Alexis123": UserInDB(**{ username = "Alexis123",
+                            correo = "alexis-tic@gmail.com",
+                            password = "clave111"}),
+
+"MiguelA12": UserInDB(**{ username = "MiguelA12",
+                            correo = "miguelangel@gmail.com",
+                            password = "clave0000"}),
+
+}
+
+
