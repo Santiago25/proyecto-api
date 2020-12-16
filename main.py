4dd5c23064c9a8d9bb21d2b911f9c3e037f57fcb
Santiago <snbedoyao@gmail.com>
2020-12-16 06:32:33 -0500

    test CORS

--- main.py
+++ main.py
@@ -12,6 +12,16 @@ from fastapi.middleware.cors import CORSMiddleware
 
 api = FastAPI()
 
+origins = [
+"http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
+"http://localhost", "http://localhost:8080",
+]
+
+api.add_middleware(
+CORSMiddleware, allow_origins=origins,
+allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
+)
+
 @api.post("/proyect/addUser/")
 async def add_user(asign_user: AsignacionIn):
 
